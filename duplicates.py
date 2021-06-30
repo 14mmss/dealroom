71e275ea3471a35936228827e08937dd94609c98
Marina <64917202+14mmss@users.noreply.github.com>
2021-06-30 17:42:16 -0300

    Add files via upload

--- duplicates.py
+++ duplicates.py
@@ -1,9 +1,13 @@
 import pandas as pd
 
+# read csv
 df = pd.read_csv('data_scientist_duplicate_detection.csv')
 
+# find duplicate rows
 duplicate_rows = df[df.duplicated()]
 
+# show rows
 print("\n\nDuplicate Rows : \n {}".format(duplicate_rows))
 
+# save them to csv file
 duplicate_rows.to_csv("duplicates.csv")
